4aca65d60bd3fbde5a5ae5bee799b4bcae5a5344
aaryavrattt <aaryavrattt@gmail.com>
2025-11-01 11:49:29 +0530

    jsdone

---
+++ js.js
@@ -0,0 +1,14 @@
+let name = "aarya";
+let age = 20;
+
+// console.log(name)
+// console.log(age)
+// console.log(name , age);
+// console.table({name, age});
+// console.log(typeof name);
+// console.log(typeof age);
+
+let a = 2;
+let b = "2";
+console.log(a==b)
+console.log(a===b);
